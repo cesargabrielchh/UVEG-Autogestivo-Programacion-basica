0977cb62d37fb55d5e042e3527820e9de14f6a0c
cesargabrielchh <cesar_gabriel89@hotmail.com>
2024-08-10 10:18:29 -0600

    I previously added a README.md file but the HelloWorld.c file  was not sent

---
+++ Hello_World.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *arvg[]) {
+    printf ("Hola mundo, este es mi primer programa en C para el curso autogestivo de la UVEG\n");
+    system("PAUSE");
+    return 0;
+}
